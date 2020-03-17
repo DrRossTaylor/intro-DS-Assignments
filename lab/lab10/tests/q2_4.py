54a164da752229b362e8af101af1297447dfe9fd
Debbie Yuster <debbieyuster@gmail.com>
2020-03-16 22:29:47 -0400

    Add files via upload

---
+++ lab/lab10/tests/q2_4.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'q1_5',
+  'hidden': True,
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 0.9586 < r_cheese_doctors < 0.9587
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
